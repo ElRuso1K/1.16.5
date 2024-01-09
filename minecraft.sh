f6818dcac969eaefcc1bf2806b6c6c785403a909
ElRuso1K <92225401+ElRuso1K@users.noreply.github.com>
2024-01-09 00:27:16 -0300

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -16,14 +16,14 @@ echo "
 
 # Ask the user for the Minecraft server script URL
 read -p "Please enter the URL of the Minecraft server script (press Enter to use the default URL): " server_url
-server_url=${server_url:-"https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/790/downloads/paper-1.16.5-790.jar"}
+server_url=${server_url:-"https://papermc.io/api/v2/projects/paper/versions/1.17/builds/66/downloads/paper-1.17-66.jar"}
 
 # Download the Minecraft server script
 wget -O server.jar $server_url
 
 # Download openjdk-16-jdk
 sudo apt-get update
-sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install openjdk-17-jdk -y
 
 # Ask the user if they accept the Minecraft EULA
 read -p "Do you accept the Minecraft EULA? (Type 'yes' or 'y' to accept): " eula_acceptance
